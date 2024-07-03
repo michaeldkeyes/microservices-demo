456075a5eccec6b64eb4725bdf95e03c58daf558
Micky <micky@Marias-iMac.local>
2024-07-03 08:40:13 -0700

    integration test

--- src/test/java/com/example/product/ProductServiceApplicationTests.java
+++ src/test/java/com/example/product/ProductServiceApplicationTests.java
@@ -1,15 +1,50 @@
 package com.example.product;
 
+import com.example.product.dto.ProductRequest;
+import io.restassured.parsing.Parser;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import io.restassured.*;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import java.math.BigDecimal;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    static MongoDBContainer mongoDbContainer = new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDbContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        final ProductRequest productRequest = new ProductRequest("iphone 13", "iphone 13", BigDecimal.valueOf(1200));
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(productRequest)
+                .when()
+                .post("/products")
+                .then()
+                .log().all()
+                .statusCode(201);
+    }
 
 }
